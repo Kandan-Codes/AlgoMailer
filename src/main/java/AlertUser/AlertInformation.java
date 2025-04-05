747b0c7b559840d934f3003f1e26762230f446fe
Kandan-Codes <kandan.codes@gmail.com>
2025-04-05 13:38:08 +0530

    Providing comment

--- src/main/java/AlertUser/AlertInformation.java
+++ src/main/java/AlertUser/AlertInformation.java
@@ -1,7 +1,9 @@
 package AlertUser;
 
 import javafx.scene.control.Alert;
-
+/*
+@Alerting: provide alert message when specific things happen
+*/
 public class AlertInformation {
     public static void showAlert(String status, String statusMessage) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
